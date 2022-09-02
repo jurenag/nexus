3f14b84a76b8d4e72ec5771ce549ff3a60fc4aae
Julio Ureña <jurenag@outlook.es>
2022-09-02 16:54:01 +0200

    Add dimples to WLSPlate geometry

--- source/geometries/WLSPlate.h
+++ source/geometries/WLSPlate.h
@@ -4,6 +4,7 @@
 #include "GeometryBase.h"
 
 #include <G4ThreeVector.hh>
+#include <G4SystemOfUnits.hh>
 
 class G4MaterialPropertiesTable;
 class G4GenericMessenger;
@@ -16,11 +17,19 @@ namespace nexus {
   {
   public:
     ///Default constructor
-    WLSPlate(G4bool with_LAr = true);
+    WLSPlate(   G4bool with_LAr = true, G4bool with_dimples = false, G4String dimple_type = "cylindrical", 
+                G4int dimples_no = 24, G4bool along_both_directions = false, G4double flat_dimple_width = 6.1*mm, 
+                G4double flat_dimple_depth = 2.*mm, G4double curvy_dimple_radius = 3.1*mm);
     ///Construct a WLSPlate with given dimensions
-    WLSPlate(G4double, G4double, G4double, G4bool with_LAr = false);
+    WLSPlate(   G4double, G4double, G4double, G4bool with_LAr = false, G4bool with_dimples = false, 
+                G4String dimple_type = "cylindrical", G4int dimples_no = 24, G4bool along_both_directions = false, 
+                G4double flat_dimple_width = 6.1*mm, G4double flat_dimple_depth = 2.*mm, 
+                G4double curvy_dimple_radius = 3.1*mm);
     ///Construct a WLSPlate with given dimensions and optical properties
-    WLSPlate(G4double, G4double, G4double, G4MaterialPropertiesTable*, G4bool with_LAr = false);
+    WLSPlate(   G4double, G4double, G4double, G4MaterialPropertiesTable*, G4bool with_LAr = false, 
+                G4bool with_dimples = false, G4String dimple_type = "cylindrical", G4int dimples_no = 24, 
+                G4bool along_both_directions = false, G4double flat_dimple_width = 6.1*mm, 
+                G4double flat_dimple_depth = 2.*mm, G4double curvy_dimple_radius = 3.1*mm);
     ///Destructor
     ~WLSPlate();
 
@@ -31,9 +40,15 @@ namespace nexus {
     G4ThreeVector GenerateVertex(const G4String&) const;
 
   private:
-    G4double dx_, dy_, dz_;             ///<WLSPlate dimensions 
-    G4MaterialPropertiesTable* mpt_;    ///<WLS optical properties
-    G4bool with_LAr_env_;               ///<Whether to build a LAr environment or not 
+    G4double dx_, dy_, dz_;                             ///< WLSPlate dimensions 
+    G4bool with_dimples_;                               ///< Whether the plate has carved dimples
+    G4String dimple_type_;                              ///< Dimple type. Might be 'flat', 'cylindrical' or 'spherical'.
+    G4int how_many_dimples_;                            ///< How many dimples to carve on EACH selected side (up to along_both_directions_) of the plate
+    G4bool along_both_directions_;                      ///< If so, dimples are carved along dx_ and dz_ sides of the plate. If not, they are only carved along the two dx_ sides.
+    G4double flat_dimple_width_, flat_dimple_depth_;    ///< Used for flat dimples. The width of the dimple (along the board direction) and its depth, perpendicular to the plate surface.
+    G4double curvy_dimple_radius_;                      ///< Used for cylindrical or spherical dimples. Radius of the dimple.
+    G4MaterialPropertiesTable* mpt_;                    ///< WLS optical properties
+    G4bool with_LAr_env_;                               ///< Whether to build a LAr environment or not 
 
     G4GenericMessenger* msg_;
   };
