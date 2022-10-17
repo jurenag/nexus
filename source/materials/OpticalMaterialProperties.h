8b07480143e7686c07d17c97d81a8712feab56db
Julio Local Laptop <jurenag@outlook.es>
2022-10-17 16:33:44 +0200

    Add MPT for a PTP volume which is immersed in LAr

--- source/materials/OpticalMaterialProperties.h
+++ source/materials/OpticalMaterialProperties.h
@@ -54,6 +54,8 @@ namespace opticalprops {
   G4MaterialPropertiesTable* LXe();
 
   G4MaterialPropertiesTable* LAr();
+
+  G4MaterialPropertiesTable* LArPTPArtifact();
   
   G4MaterialPropertiesTable* PerfectPhotonCollector();
 
