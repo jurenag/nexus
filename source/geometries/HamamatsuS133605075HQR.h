9f474b63320de6cec570fb42e0b2531e666c3cdf
Julio Ureña <jurenag@outlook.es>
2023-02-14 16:32:38 +0100

    Add SiPM-MPPC model for Hamamatsu S13360-5075-HQR

---
+++ source/geometries/HamamatsuS133605075HQR.h
@@ -0,0 +1,32 @@
+#ifndef HAMAMATSU_S13360_5075_HQR_H
+#define HAMAMATSU_S13360_5075_HQR_H
+
+#include "SiPMMPPC.h"
+
+namespace nexus {
+
+  class HamamatsuS133605075HQR: public SiPMMPPC
+  {
+  public:
+    // Constructor for a Hamamatsu S13360 5075 HQR MPPC
+    HamamatsuS133605075HQR();
+
+    // Destructor
+    ~HamamatsuS133605075HQR();
+
+    G4double const GetTransverseDim()         override  { return 6.4*mm;          }   
+    G4double const GetSensareaTransverseDim() override  { return 6.0*mm;          }
+    G4double const GetThickness()             override  { return 1.5*mm;          } // This one differs from HamamatsuS133606050VE
+    G4double const GetWindowThickness()       override  { return 0.15*mm;         } // This one differs from HamamatsuS133606050VE  
+    G4double const GetSensareaThickness()     override  { return 0.1*mm;          }
+    G4String const GetModel()                 override  { return "HS133605075HQR";}
+
+  private:
+    std::pair<G4int, G4double*> GetSensareaEnergyArray() override;
+    std::pair<G4int, G4double*> GetSensareaEfficiencyArray() override;
+    std::pair<G4int, G4double*> GetSensareaReflectivityArray() override;
+  };
+
+} // namespace nexus
+
+#endif
