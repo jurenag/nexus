7751e929da17b8269840ac27e9cb0ec591f44e14
Julio Ureña <jurenag@outlook.es>
2023-02-14 16:46:48 +0100

    Adapt class SiPMBoard to the new class hierarchy for SiPM MPPCs

--- source/geometries/SiPMBoard.h
+++ source/geometries/SiPMBoard.h
@@ -17,17 +17,22 @@ namespace nexus {
 
     inline void SetBaseID(G4int input)              { base_id_                  = input; }
     inline void SetNumPhsensors(G4int input)        { num_phsensors_            = input; }
+    inline void SetSiPMCode(G4int input)            { SiPM_code_                = input; }
     inline void SetBoardLength(G4double input)      { board_length_             = input; }
     inline void SetBoardHeight(G4double input)      { board_height_             = input; }
     inline void SetBoardThickness(G4double input)   { board_thickn_             = input; }
     inline void SetReflectiveSupports(G4bool input) { ref_phsensors_supports_   = input; }
 
     G4int GetNumPhsensors()         const;  ///< Returns the number of photosensors attached to the board 
+    G4int GetSiPMCode()             const;  ///< Returns the SiPM code
     G4double GetBoardLength()       const;  ///< Returns the length of the board
     G4double GetBoardHeight()       const;  ///< Returns the height of the board
-    G4double GetOverallHeight()      const;  ///< Returns the span of the geometry along the height axis (i.e. the sipm height if the sipm heighth is greater than the board height, and the board height otherwise)
+    G4double GetOverallHeight()     const;  ///< Returns the span of the geometry along the height axis 
+                                            ///< (i.e. the sipm height if the sipm heighth is greater 
+                                            ///< than the board height, and the board height otherwise)
     G4double GetBoardThickness()    const;  ///< Returns the thickness of the board alone
-    G4double GetOverallThickness()  const;  ///< Returns the span of the geometry along the thickness axis (i.e. the sum of the thickness of the board plus the SiPM thickness)
+    G4double GetOverallThickness()  const;  ///< Returns the span of the geometry along the thickness axis 
+                                            ///< (i.e. the sum of the thickness of the board plus the SiPM thickness)
 
     G4bool GeometryIsIllFormed() const;     ///< Whether the provided parameters describe a feasible geometry
 
@@ -37,8 +42,13 @@ namespace nexus {
 
   private:
 
-    G4int base_id_;                     ///< ID of the first SiPM that is mounted. The i-th SiPM after the first one has an ID equal to base_id_+i
+    G4int base_id_;                     ///< ID of the first SiPM that is mounted. The i-th SiPM after the first 
+                                        ///< one has an ID equal to base_id_+i
     G4int num_phsensors_;               ///< Number of SiPMs that are mounted on the board
+    G4int SiPM_code_;                   ///< Integer signalling which SiPM to mount on the board
+                                        ///< 1                  -> Hamamatsu S13360-6050VE
+                                        ///< 2                  -> Hamamatsu S13360-5075HD-HQR
+                                        ///< Any other integer  -> FBK-NUV-HD-CRYO-TT
     G4double board_thickn_;             ///< Thickness of the mounting board
     G4double board_length_;             ///< Board length
     G4double board_height_;             ///< Board height (width)
