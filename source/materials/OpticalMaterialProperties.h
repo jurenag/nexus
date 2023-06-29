b708404e45a59ebc83aeb2d9a8295a5ef4e81f8d
Julio Ureña <jurenag@outlook.es>
2023-06-29 13:21:50 +0200

    Add two changes to G2P_FB118 material properties table
    The first change has to do with the refractive index. As of now, it can be set (to a constant) via an input parameter. The second change has to do with the absorption length spectra, namely the WLS absorption length and the attenuation length (bulk absorption). Different spectra can be chosen up to the value of an input parameter, which reflects the WLS dye concentration in milligrams of WLS dye per PMMA kilogram.

--- source/materials/OpticalMaterialProperties.h
+++ source/materials/OpticalMaterialProperties.h
@@ -91,7 +91,7 @@ namespace opticalprops {
 
   G4MaterialPropertiesTable* EJ286(G4double attenuation_length=1.*m);
 
-  G4MaterialPropertiesTable* G2P_FB118(G4double attenuation_length);
+  G4MaterialPropertiesTable* G2P_FB118(G4double cromophore_concentration, G4double rindex=1.502, G4bool verbosity = true);
 
   G4MaterialPropertiesTable* SCHOTT_B270();
 
---
+++ source/utils/NumericalUtils.h
@@ -0,0 +1,12 @@
+#ifndef NUMERICAL_UTILS_H
+#define NUMERICAL_UTILS_H
+
+#include <globals.hh>
+
+namespace nexus {
+
+    G4double FindClosestNumber(G4double, std::vector<G4double>*);
+    
+}
+
+#endif
