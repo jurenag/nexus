fc90478968e677340a9dd8162887624ab67732fe
Julio <jurenag@outlook.es>
2023-12-21 16:13:36 +0100

    Propagate SiPMMPPC changes to HamamatsuS133605075HQR class

--- source/geometries/HamamatsuS133605075HQR.h
+++ source/geometries/HamamatsuS133605075HQR.h
@@ -14,12 +14,18 @@ namespace nexus {
     // Destructor
     ~HamamatsuS133605075HQR();
 
-    G4double const GetTransverseDim()         override  { return 6.4*mm;          }   
-    G4double const GetSensareaTransverseDim() override  { return 6.0*mm;          }
-    G4double const GetThickness()             override  { return 1.5*mm;          } // This one differs from HamamatsuS133606050VE
-    G4double const GetWindowThickness()       override  { return 0.15*mm;         } // This one differs from HamamatsuS133606050VE  
-    G4double const GetSensareaThickness()     override  { return 0.1*mm;          }
-    G4String const GetModel()                 override  { return "HS133605075HQR";}
+    G4double GetTransverseDim()           override { return 6.4   *mm       ; }
+    G4int GetSiPMsNoAlongX()              override { return 1               ; }
+    G4int GetSiPMsNoAlongZ()              override { return 1               ; }
+    G4double GetOuterFrameWidthAlongX()   override { return 0.2   *mm       ; }
+    G4double GetOuterFrameWidthAlongZ()   override { return 0.2   *mm       ; }
+    G4double GetInnerFramesWidthAlongX()  override { return 0.0             ; }
+    G4double GetInnerFramesWidthAlongZ()  override { return 0.0             ; }
+    G4double GetThickness()               override { return 1.5   *mm       ; } ///< This one differs from HamamatsuS133606050VE
+    G4double GetWindowThickness()         override { return 0.15  *mm       ; } ///< This one differs from HamamatsuS133606050VE
+    G4double GetSensareaThickness()       override { return 0.1   *mm       ; }
+    G4String GetModel()                   override { return "HS133605075HQR"; } ///< This one differs from HamamatsuS133606050VE
+
 
   private:
     G4int GetNPoints() override { return 67;}
