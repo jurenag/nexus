04e8df6d80503bf7ba183bdb6f8570a9af037cd2
Julio <jurenag@outlook.es>
2025-05-15 13:17:43 +0200

    Minimal changes to WLSPlate.h documentation

--- source/geometries/WLSPlate.h
+++ source/geometries/WLSPlate.h
@@ -87,16 +87,20 @@ namespace nexus {
     G4bool dimples_at_z_minus_;                         ///< Whether to carve dimples at the WLSPlate side which is contained within z<0.0
     G4String dimple_type_;                              ///< Dimple type. Might be 'flat', 'cylindrical' or 'spherical'.
     G4int how_many_dimples_;                            ///< How many dimples to carve on EACH selected side of the plate
-    G4double flat_dimple_width_, flat_dimple_depth_;    ///< Used for flat dimples. The width of the dimple (along the board direction) and its depth, perpendicular to the plate surface.
+    G4double flat_dimple_width_, flat_dimple_depth_;    ///< Used for flat dimples. The width of the dimple (along the board direction)
+                                                        ///< and its depth, perpendicular to the plate surface.
     G4double curvy_dimple_radius_;                      ///< Used for cylindrical or spherical dimples. Radius of the dimple.
     G4bool cut_plate_;                                  ///< Whether to split the WLS plate up into two pieces
                                                         ///< WARNING: Although the application works fine, the visualization may
                                                         ///<          break down when dimples are used and cut_plate_ is true.
-    G4double cut_angle_;                                ///< This parameter only makes a difference if cut_plate_ is true. Angle of the cut, with respect to the Z axis (the axis along which the WLS plate is dz_ long)
-    G4double cut_thickness_;                            ///< This parameter only makes a difference if cut_plate_ is true. Thickness of the cut/crack that is carved from the plate.
-    G4double tunneling_probability_;                    ///< Probability that a photon tunnels through the surface of the WLSPlate (inwards or outwards), meaning that it is 
-                                                        ///< straightfoward-ly transmitted (no deflection due to Frensel refraction). The goal of this parameter is to let 
-                                                        ///< us model surface imperfections of the WLS plate.
+    G4double cut_angle_;                                ///< This parameter only makes a difference if cut_plate_ is true. Angle of the cut,
+                                                        ///< with respect to the Z axis (the axis along which the WLS plate is dz_ long)
+    G4double cut_thickness_;                            ///< This parameter only makes a difference if cut_plate_ is true. Thickness of the
+                                                        ///< cut/crack that is carved from the plate.
+    G4double tunneling_probability_;                    ///< Probability that a photon tunnels through the surface of the WLSPlate (inwards
+                                                        ///< or outwards), meaning that it is straightfoward-ly transmitted (no deflection
+                                                        ///< due to Frensel refraction). The goal of this parameter is to let the user
+                                                        ///< model surface imperfections of the WLS plate.
     G4double generation_y_pos_;                         ///< The y-position of the generation vertex of photons.
     G4String generation_mode_;                          ///< Controls the generation vertex of photons. It can take the following values
                                                         ///< "random"       - The x- and z-positions of the generation vertex are randomly sampled 
